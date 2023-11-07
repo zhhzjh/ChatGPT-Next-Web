deb374c9b0f58823e884504f4ba1d9703c1c7ec4
forkyguo <13811831379@qq.com>
2023-11-07 17:25:27 +0800

    插件

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -168,13 +168,13 @@ export function SideBar(props: { className?: string }) {
           }}
           shadow
         />
-        <IconButton
+        {/* <IconButton
           icon={<PluginIcon />}
           text={shouldNarrow ? undefined : Locale.Plugin.Name}
           className={styles["sidebar-bar-button"]}
           onClick={() => showToast(Locale.WIP)}
           shadow
-        />
+        /> */}
       </div>
 
       <div

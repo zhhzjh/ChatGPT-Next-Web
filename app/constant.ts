8dae06437def0faae733e1228b081b1f7bfd54f4
forkyguo <13811831379@qq.com>
2023-11-07 16:40:01 +0800

    STORAGE_KEY

--- app/constant.ts
+++ app/constant.ts
@@ -53,7 +53,7 @@ export const ACCESS_CODE_PREFIX = "nk-";
 export const LAST_INPUT_KEY = "last-input";
 export const UNFINISHED_INPUT = (id: string) => "unfinished-input-" + id;
 
-export const STORAGE_KEY = "chatgpt-next-web";
+export const STORAGE_KEY = "ruxi";
 
 export const REQUEST_TIMEOUT_MS = 60000;
 
